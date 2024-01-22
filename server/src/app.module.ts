01e71a9a0caa76ddc8318a74548e491fb5218ba0
R3D1NM <r3dinm@gmail.com>
2024-01-22 23:11:09 +0900

    [build] db connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
+import { User } from './entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
