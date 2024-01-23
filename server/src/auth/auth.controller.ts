68ee95de741f46d4f240aa9b62798cfdf8c8a15e
R3D1NM <r3dinm@gmail.com>
2024-01-23 23:26:29 +0900

    [feat] create auth dto

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository : UserRepository
+    ){}
+
+}
