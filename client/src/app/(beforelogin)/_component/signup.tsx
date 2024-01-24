c56c0e0f55a8ec23438c32ae07f49e0fa3cfcdce
R3D1NM <r3dinm@gmail.com>
2024-01-24 18:53:34 +0900

    [feat] signup validation message

--- client/src/app/(beforelogin)/_component/signup.tsx
+++ client/src/app/(beforelogin)/_component/signup.tsx
@@ -7,10 +7,19 @@ export default function SignUp(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-    const [message, setMessage] = useState([]);
+    const [message, setMessage] = useState({
+        email:"",
+        password:"",
+        username:""
+    });
 
     const onSignup = async (e) =>{
         e.preventDefault()
+        setMessage({
+            email: "",
+            password: "",
+            username: "",
+        });
         const payload = {
             username,
             password,
@@ -24,8 +33,24 @@ export default function SignUp(props) {
             
         })
         .catch((err)=>{
-            console.log(err.response.data.message);
-            setMessage(err.response.data.message)
+            console.log(err);
+            if(err.response.status===400){
+                const errors = err.response.data.message
+                errors?.forEach(e => {
+                    console.log(e);
+                    if (e.includes("username")) {
+                        setMessage((prev) => ({ ...prev, username: e }));
+                    } else if (e.includes("password")) {
+                        setMessage((prev) => ({ ...prev, password: e }));
+                    } else if (e.includes("email")) {
+                        setMessage((prev) => ({ ...prev, email: e }));
+                    }
+                });
+            } else if (err.response.status===409){
+                const msg = err.response.data.message
+                setMessage((prev) => ({ ...prev, username: msg }));
+            }
+
         })
     }
 
@@ -40,30 +65,29 @@ export default function SignUp(props) {
             <Form onSubmit={onSignup}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <Form.Control type="email" placeholder="Enter email" required value={email} onChange={(e)=>setEmail(e.target.value)} isInvalid={Boolean(message.email!=="")}/>
+                    <Form.Control.Feedback type="invalid">{message.email}</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="input" placeholder="Username" required value={username} onChange={(e)=>setUsername(e.target.value)}/>
+                    <Form.Control type="input" placeholder="Username" required value={username} onChange={(e)=>setUsername(e.target.value)} isInvalid={Boolean(message.username!=="")}/>
+                    <Form.Control.Feedback type="invalid">{message.username}</Form.Control.Feedback>
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <Form.Control type="password" placeholder="Password" required value={password} onChange={(e)=>setPassword(e.target.value)} isInvalid={Boolean(message.password!=="")}/>
+                    <Form.Control.Feedback type="invalid">{message.password}</Form.Control.Feedback>
                 </Form.Group>
-                <div className="tw-flex tw-flex-col tw-justify-between">
-                    <Button variant="primary" type="submit">
-                        Submit
+
+                <div className="tw-flex tw-justify-end">
+                    <Button variant="primary" type="submit" className="">
+                        Join
                     </Button>
-                    <div className="tw-pr-2">
-                        {message.map((msg => {
-                            return <small className="tw-text-red-600">
-                                {msg} <br/>
-                            </small>
-                        }))}
-                    </div>
                 </div>
+
             </Form>
 
             </Modal.Body>
